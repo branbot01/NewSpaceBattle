6de7067b8b86baac1486b445e3e6206df77b939c
super <shredder131517@gmail.com>
2023-06-29 10:51:40 -0400

    fixed ships over velocity issue

--- app/src/main/java/com/newspacebattle/GameObject.java
+++ app/src/main/java/com/newspacebattle/GameObject.java
@@ -31,10 +31,12 @@ class GameObject {
 
     //Method used to update position if the object has velocity
     void move() {
+        float previousVelocityX = velocityX;
+        float previousVelocityY = velocityY;
         velocityX += accelerationX;
         velocityY += accelerationY;
 
-        if (Math.sqrt(Math.pow(velocityX, 2) + Math.pow(velocityY, 2)) >= maxSpeed) {
+        if (Math.sqrt(Math.pow(velocityX, 2) + Math.pow(velocityY, 2)) >= maxSpeed && Math.abs(velocityX) > Math.abs(previousVelocityX) && Math.abs(velocityY) > Math.abs(previousVelocityY)) {
             velocityX -= accelerationX;
             velocityY -= accelerationY;
         }
@@ -53,11 +55,11 @@ class GameObject {
     public String toString() {
         System.out.println("Object Type: " + type);
         System.out.println("Position: " + centerPosX + ", " + centerPosY);
-        //System.out.println("Velocity: " + velocityX + ", " + velocityY);
+        System.out.println("Velocity: " + velocityX + ", " + velocityY);
         //System.out.println("grav: " + gravVelX + ", " + gravVelY);
         System.out.println("Acceleration: " + accelerationX + ", " + accelerationY);
         System.out.println("Degrees/Radius: " + degrees + ", " + radius);
-        //System.out.println("Velocity Angle: " + Utilities.angleDim(velocityX, velocityY));
+        System.out.println("Velocity Angle: " + Utilities.angleDim(velocityX, velocityY));
         //System.out.println("Rotated point + 90: " + Utilities.circleAngleX(degrees + 90, centerPosX, radius) + ", " + Utilities.circleAngleY(degrees + 90, centerPosY, radius));
         return "";
     }
