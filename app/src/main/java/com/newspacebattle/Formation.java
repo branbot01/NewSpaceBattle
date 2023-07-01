9426e149b054e1ca4ca3e354dd035253d8a6b4db
branbot01 <branbot01@gmail.com>
2023-07-01 00:12:12 -0400

    Added rectangle formation, needs testing

--- app/src/main/java/com/newspacebattle/Formation.java
+++ app/src/main/java/com/newspacebattle/Formation.java
@@ -2,16 +2,26 @@ package com.newspacebattle;
 
 import java.util.ArrayList;
 
+/**
+ * Created by Dylan and Brandon on 2023-06-29. Defines a formation, which is a structured group of ships.
+ */
 class Formation {
 
     ArrayList<Ship> ships;
     double centerX, centerY;
+    int type;
 
-    Formation(ArrayList<Ship> ships) {
+    //Constructor method
+    Formation(ArrayList<Ship> ships, int type) {
+        this.type = type;
         this.ships = ships;
         setCenter();
+        if (type == 0) {
+            rectangleFormation();
+        }
     }
 
+    //Finds the center of the selected ships
     void setCenter() {
         double sumX = 0, sumY = 0, sumMass = 0;
         for (int i = 0; i < ships.size(); i++) {
@@ -21,5 +31,45 @@ class Formation {
         }
         centerX = sumX / sumMass;
         centerY = sumY / sumMass;
+    }
+
+    void rectangleFormation() {
+        int shipCounter = 0;
+
+        for (int i = 0; i < ships.size(); i++) {
+            if (ships.get(i) instanceof BattleShip || ships.get(i) instanceof FlagShip) {
+                if (shipCounter == 0) {
+                    ships.get(i).setDestination((float) centerX, (float) centerY, false);
+                } else {
+                    ships.get(i).setDestination((float) centerX + (ships.get(i - 1).avoidanceRadius) * (float) Math.pow(-1, shipCounter), (float) centerY, false);
+                }
+                shipCounter++;
+            }
+        }
+
+        for (int i = 0; i < ships.size(); i++) {
+            if (ships.get(i) instanceof LaserCruiser) {
+                if (shipCounter == 0) {
+                    ships.get(i).setDestination((float) centerX, (float) centerY, false);
+                } else {
+                    ships.get(i).setDestination((float) centerX + (ships.get(i - 1).avoidanceRadius) * (float) Math.pow(-1, shipCounter), (float) centerY, false);
+                }
+                shipCounter++;
+            }
+        }
+
+        for (int i = 0; i < ships.size(); i++) {
+            if (ships.get(i) instanceof Fighter || ships.get(i) instanceof Bomber || ships.get(i) instanceof Scout || ships.get(i) instanceof ResourceCollector) {
+                if (shipCounter == 0){
+                    ships.get(i).setDestination((float) centerX, (float) (centerY + ((Fighter.constRadius * 10 * Math.sqrt(3)) / 4)), false);
+                } else {
+                    if (shipCounter % 3 == 0) {
+                        ships.get(i).setDestination((float) ((centerX + ((Fighter.constRadius * 10 * Math.sqrt(3)) / 4)) * (float) Math.pow(-1, shipCounter)), (float) (centerY - ((Fighter.constRadius * 10 * Math.sqrt(3)) / 4)), false);
+                    }else {
+                        ships.get(i).setDestination((float) ((centerX + ((Fighter.constRadius * 10 * Math.sqrt(3)) / 4)) * (float) Math.pow(-1, shipCounter)), (float) (centerY + ((Fighter.constRadius * 10 * Math.sqrt(3)) / 4)), false);
+                    }
+                }
+            }
+        }
     }
 }
